9607a0cb747d76509703eb0fc99f39e3a1a96c7a
hukl <contact@smyck.org>
2010-11-19 14:26:17 +0100

    added production environment

--- init.rb
+++ init.rb
@@ -7,6 +7,16 @@ require 'cached_file'
 require 'fileutils'
 require 'uuid'
 
+configure :production do
+  puts ">>>>>>>>>>>>>>>> PRODUCTION <<<<<<<<<<<<<<<<<"
+  Mongoid.configure do |config|
+    name = "hoccer_development"
+    host = "localhost"
+    config.master = Mongo::Connection.new.db(name)
+    config.persist_in_safe_mode = true
+  end
+end
+
 configure :development do
   puts ">>>>>>>>>>>>>>>> DEVELOPMENT <<<<<<<<<<<<<<<<<"
   Mongoid.configure do |config|
